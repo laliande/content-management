941aa6ad7e57683153ea08cb88adf9b09de93140
Daniella Starchenko <laliande7@gmail.com>
2020-09-20 00:24:35 +0300

    added file with data

--- public.py
+++ public.py
@@ -31,13 +31,19 @@ data = {
 }
 
 
-def publish(data):
-    URL = 'https://www.haroldltd.ru/cms2/cat/inscore.php'
-    headers = set_publish_headers()
-    cookies = login()
-    response = requests.post(
-        url=URL, data=data, headers=headers, cookies=cookies)
-    print(response.text)
+def get_data():
+    data = []
+    with open('data.json', 'r', encoding='utf-8') as fh:
+        data = json.load(fh)
+    return data
 
 
-publish(data)
+def publish():
+    data = get_data()
+    for item in data:
+        URL = 'https://www.haroldltd.ru/cms2/cat/inscore.php'
+        headers = set_publish_headers()
+        cookies = login()
+        response = requests.post(
+            url=URL, data=item, headers=headers, cookies=cookies)
+        print(response.text)
